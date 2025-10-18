c1b0a44a7b10688cbd4da440f88c88bd5c24947c
3nda <jan@smid.sk>
2025-10-18 16:23:53 +0200

    fix electric/fan.gd

--- godot/electric_fan/electric_fan.gd
+++ godot/electric_fan/electric_fan.gd
@@ -3,21 +3,25 @@ extends Node2D
 @export var is_on: bool = false
 @export var speed: FanSpeed = FanSpeed.Normal
 @onready var sprites = $AnimatedSprite2D
+@onready var connector = $ElectricConnector
 enum FanSpeed {Slow, Normal, Fast}
 var is_on_prev: bool = is_on
 
 func _ready() -> void:
 	set_speed(speed)
-	if is_on:
-		start()
+	on_off_changed(is_on)
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	if is_on != is_on_prev:
 		is_on_prev = is_on
 		on_off_changed(is_on)
 
-func on_off_changed(current: bool) -> void:
-	print_debug("electric/fan: %s" % ["ON" if current else "off"])
+func on_off_changed(value: bool) -> void:
+	print_debug("electric/fan: %s - GFX" % connector.on_off_str(value))
+	if value:
+		start()
+	else:
+		stop()
 
 func start() -> void:
 	is_on = true
@@ -45,7 +49,7 @@ func _physics_process(delta: float) -> void:
 	for body in $Area2D.get_overlapping_bodies():
 		if body is RigidBody2D:
 			var force: int
-			
+
 			match speed:
 				FanSpeed.Slow:
 					force = 50
@@ -53,8 +57,11 @@ func _physics_process(delta: float) -> void:
 					force = 100
 				FanSpeed.Fast:
 					force = 200
-					
+
 			force *= clamp(1 - global_position.distance_to(body.global_position) / 300, 0, 1)
-					
+
 			body.apply_central_force(Vector2(force, 0).rotated(rotation) * sign(scale.x))
-		
+
+func _on_on_off_changed(value: bool) -> void:
+	print_debug("electric/fan: connector(%s)" % connector.on_off_str(value))
+	is_on = value
