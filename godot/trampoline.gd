45cfdddafe5c6034fee56bd22cc57ef39927f671
Vendula Němcová <v.nem.co@gmail.com>
2025-10-18 21:05:57 +0200

    Add trampoline assets.

--- godot/trampoline.gd
+++ godot/trampoline.gd
@@ -1,5 +1,6 @@
 extends RigidBody2D
 
+@onready var sprites: AnimatedSprite2D = $AnimatedSprite2D
 
 func _on_bouncy_area_body_entered(body: Node2D) -> void:
 	if not body is RigidBody2D:
@@ -9,3 +10,5 @@ func _on_bouncy_area_body_entered(body: Node2D) -> void:
 	var top_normal := Vector2.UP.rotated(global_rotation)
 
 	body.linear_velocity = rel_vel.bounce(top_normal) * 2.0
+	
+	sprites.play("default")
