b6585b9ca59ed0d8c4c7c56703c8063419a7743b
3nda <jan@smid.sk>
2025-10-18 13:33:17 +0200

    add collision: electric/switch x ball -> is switching on/off

--- godot/electric_switch/electric_switch.gd
+++ godot/electric_switch/electric_switch.gd
@@ -14,10 +14,20 @@ func _process(_delta: float) -> void:
 		on_off_changed(is_on)
 
 func on_off_changed(current: bool) -> void:
-	print_debug("electric/switch: %s" % ["ON" if current else "off"])
+	print_debug("electric/switch: %s" % on_off_str(is_on))
 	if current:
 		spriteOn.visible = true
 		spriteOff.visible = false
 	else:
 		spriteOn.visible = false
 		spriteOff.visible = true
+
+func _on_area_2d_body_entered(_body: Node2D) -> void:
+	print_debug("electric/switch: collission ... %s >> %s" % [
+		on_off_str(is_on),
+		on_off_str(!is_on)
+	])
+	is_on = !is_on
+
+func on_off_str(value: bool) -> String:
+	return "ON" if value else "off"
