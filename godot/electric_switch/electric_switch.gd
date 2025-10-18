ae8f1ead05d9e495c006910e6eccae8f4985b078
3nda <jan@smid.sk>
2025-10-18 15:36:39 +0200

    fix electric_switch

--- godot/electric_switch/electric_switch.gd
+++ godot/electric_switch/electric_switch.gd
@@ -1,11 +1,14 @@
+class_name ElectricSwitch
 extends Node2D
 
 @export var is_on: bool = false
 @onready var spriteOn = $"Sprite2D-on"
 @onready var spriteOff = $"Sprite2D-off"
+@onready var connector = $ElectricConnector
 var is_on_prev: bool = is_on
 
 func _ready() -> void:
+	set_state(is_on)
 	on_off_changed(is_on)
 
 func _process(_delta: float) -> void:
@@ -14,7 +17,7 @@ func _process(_delta: float) -> void:
 		on_off_changed(is_on)
 
 func on_off_changed(current: bool) -> void:
-	print_debug("electric/switch: %s" % on_off_str(is_on))
+	print_debug("electric/switch: %s - GFX" % connector.on_off_str(is_on))
 	if current:
 		spriteOn.visible = true
 		spriteOff.visible = false
@@ -24,10 +27,12 @@ func on_off_changed(current: bool) -> void:
 
 func _on_area_2d_body_entered(_body: Node2D) -> void:
 	print_debug("electric/switch: collission ... %s >> %s" % [
-		on_off_str(is_on),
-		on_off_str(!is_on)
+		connector.on_off_str(is_on),
+		connector.on_off_str(!is_on)
 	])
-	is_on = !is_on
+	set_state(!is_on)
 
-func on_off_str(value: bool) -> String:
-	return "ON" if value else "off"
+func set_state(value: bool) -> void:
+	is_on = value
+	print_debug("electric/switch: %s - LOGIC" % connector.on_off_str(is_on))
+	connector.on_off_changed.emit(is_on)
