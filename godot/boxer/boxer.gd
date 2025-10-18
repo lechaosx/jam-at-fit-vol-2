59b471d421f7c1774dba623aba3c950c23cab6a4
Drahomír Dlabaja <lechaosx@gmail.com>
2025-10-18 19:20:20 +0200

    boxerka refactor

--- godot/boxer/boxer.gd
+++ godot/boxer/boxer.gd
@@ -6,10 +6,14 @@ extends Node2D
 var is_punched: bool = false
 
 func trigger_me() -> void:
-	print_debug("boxer/PUNCH >> >> >> ...")
-	is_punched = true
-	aplayer.play("moveToPunch")
-	pass
+	if not is_punched:
+		
+		print_debug("boxer/PUNCH >> >> >> ...")
+		is_punched = true
+		$"go-go-push-me".freeze = false
+		$"go-go-push-me".apply_central_impulse(Vector2.LEFT.rotated(global_rotation) * 1000)
+	#	aplayer.play("moveToPunch")
+		pass
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if is_punched:
