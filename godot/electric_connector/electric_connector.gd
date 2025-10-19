acd77b5bab01f2dbcd5220e6e63fad9e58f0d213
Eduard Frlicka <edo.frlicka@gmail.com>
2025-10-19 14:19:27 +0200

    El Connector

--- godot/electric_connector/electric_connector.gd
+++ godot/electric_connector/electric_connector.gd
@@ -3,11 +3,17 @@ extends Node2D
 
 signal on_off_changed(value: bool)
 signal propagate_state_cahnged(value:bool)
+# Will be called twice per cable connection
+signal on_connected()
 
 var source
 @onready var static_body:StaticBody2D = $StaticBody2D
 
 func state_changed(value:bool):
+	propagate_state_cahnged.emit(value)
+
+func set_state(value:bool):
+	on_off_changed.emit(value)
 	propagate_state_cahnged.emit(value)
 
 func set_source(_source: ElecticConnector) -> void:
@@ -23,6 +29,8 @@ func set_source(_source: ElecticConnector) -> void:
 			self.get_parent().name
 		])
 		_source.on_off_changed.connect(state_changed)
+		_source.on_connected.emit()
+		on_connected.emit()
 
 func has_source() -> bool:
 	return true if source != null else false
